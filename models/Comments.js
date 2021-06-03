0f4d85771f233c4af562bbb565c8fc1d9f8bcb81
syed Rizvi <rizvi5a@yahoo.com>
2021-06-02 20:56:23 -0500

    added code to models

---
+++ models/Comments.js
@@ -0,0 +1,34 @@
+class Comments extends Model {}
+
+Comments.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    commenttext: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+   
+   
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'project',
+  }
+);
+
+module.exports = Comments;
