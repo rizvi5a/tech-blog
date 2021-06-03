94973ae0941ac7dd3b031ad335cd26104dcc364e
syed Rizvi <rizvi5a@yahoo.com>
2021-06-02 21:21:45 -0500

    setting uo routes

--- server.js
+++ server.js
@@ -28,6 +28,9 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(require("./controllers/api/userRoutes"));
+// app.use(require("./controllers/api/blogRoutes"))
+// app.use(require("./controllers/homeRoutes.js"))
 
 app.use(routes);
 
